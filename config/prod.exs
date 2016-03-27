b8bf141917e4aeb4bb3dcb35a07a8e949841fb5e
Philip Müller <mail@philip.in-aachen.net>
2016-03-28 00:24:56 +0200

    try to set correct host

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :scripture, Scripture.Endpoint,
   http: [port: 4800],
-  url: [host: "example.com", port: 80],
+  url: [host: "blog.hullubullu.de"],
   cache_static_manifest: "priv/static/manifest.json",
   server: true,
   root: "."
