2ab82b8be27aa74687a98fad916c804609e9bcfd
Philip Müller <mail@philip.in-aachen.net>
2016-08-21 14:02:43 -0400

    fix production config, enable server (d'oh)

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :scripture, Scripture.Endpoint,
   http: [port: 4800],
   url: [host: "scripture.hullubullu.de"],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
