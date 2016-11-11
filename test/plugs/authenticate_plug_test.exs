90365c7a4784549df7c365453305f8c7867f20b8
Philip Müller <mail@philip.in-aachen.net>
2016-11-10 22:23:53 -0500

    add an actual authentication test

--- test/plugs/authenticate_plug_test.exs
+++ test/plugs/authenticate_plug_test.exs
@@ -1,22 +1,31 @@
 defmodule Scripture.AuthenticatePlugTest do
   use Scripture.ConnCase, async: true
 
-  @opts Scripture.AuthenticatePlug.init("reader")
+  @opts Scripture.AuthenticatePlug.init("admin")
 
   test "denies access if no logged in user" do
     conn = browser_conn()
     |> get("/admin/articles")
-    |> Phoenix.Controller.accepts(["html"])
     |> Scripture.AuthenticatePlug.call(@opts)
 
     assert conn.status == 403
     assert String.contains?(get_flash(conn, :info), "Bitte logge dich ein")
   end
 
-  test "does not trigger redirect if logged in user" do
+  test "denies access if unauthorized user" do
+    conn = browser_conn()
+    |> get("/admin/articles")
+    |> log_in_as(persist_fixture(:user))
+    |> Scripture.AuthenticatePlug.call(@opts)
+
+    assert conn.status == 403
+    assert String.contains?(get_flash(conn, :error), "Du bist nicht berechtigt, auf diese Seite zuzugreifen.")
+  end
+
+  test "does not halt connection if logged in authorized user" do
     conn = browser_conn()
       |> get("/")
-      |> log_in_as(persist_fixture(:user))
+      |> log_in_as(persist_fixture(:admin))
       |> Scripture.AuthenticatePlug.call(@opts)
 
     assert conn.status == nil # no answer yet
