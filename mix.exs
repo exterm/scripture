6223c49f7a662149d00f7847789c0a39df33eee9
Philip Müller <mail@philip.in-aachen.net>
2016-11-13 17:23:08 -0500

    make sure to load earmark

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Scripture.Mixfile do
   def application do
     [mod: {Scripture, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :phoenix_swoosh, :rollbax, :edeliver]]
+                    :phoenix_ecto, :postgrex, :phoenix_swoosh, :rollbax, :edeliver, :earmark]]
   end
 
   # Specifies which paths to compile per environment.
