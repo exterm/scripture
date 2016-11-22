1d14a375c5c9b99856f190827c725cc5d39f77fb
Philip Müller <mail@philip.in-aachen.net>
2016-11-21 23:04:36 -0500

    change email sender address

--- web/emails/user_email.ex
+++ web/emails/user_email.ex
@@ -4,7 +4,8 @@ defmodule Scripture.UserEmail do
   def login_token(user) do
     new
     |> to({user.first_name <> " " <> user.last_name, user.email})
-    |> from({"Anna und Philip", "annaundphilip-blog@philip.in-aachen.net"})
+    |> from({"Anna und Philip", "blog@annaundphilip.info"})
+    |> reply_to("annaundphilip-blog@philip.in-aachen.net")
     |> subject("Login bei Anna und Philips Blog")
     |> render_body("login_token.html", %{token: user.login_token})
   end
