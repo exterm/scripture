7176824b2159d86ab849e653cb419840b367ab22
Philip Müller <mail@philip.in-aachen.net>
2016-12-06 08:03:37 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Scripture.Mixfile do
 
   def project do
     [app: :scripture,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
