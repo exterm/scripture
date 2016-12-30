5095d743a7ad04f98fe2f7ee2f685701e9faaccd
Philip Müller <mail@philip.in-aachen.net>
2016-12-30 12:14:30 +0100

    remove reply-to workaround

--- web/emails/user_email.ex
+++ web/emails/user_email.ex
@@ -5,7 +5,6 @@ defmodule Scripture.UserEmail do
     new
     |> to({user.first_name <> " " <> user.last_name, user.email})
     |> from({"Anna und Philip", "blog@annaundphilip.info"})
-    |> reply_to("annaundphilip-blog@philip.in-aachen.net")
     |> subject("Login bei Anna und Philips Blog")
     |> render_body("login_token.html", %{token: user.login_token})
   end
