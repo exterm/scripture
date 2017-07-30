da125f9e1ffaaa6c14a1654a1d92624f9061d739
Philip Müller <mail@philip.in-aachen.net>
2017-07-29 22:47:38 -0400

    if an admin writes a comment, don't send an email about that to him.

--- test/controllers/comment_controller_test.exs
+++ test/controllers/comment_controller_test.exs
@@ -15,16 +15,29 @@ defmodule Scripture.CommentControllerTest do
     admin = persist_fixture(User, :admin)
     admin2 = persist_fixture(User, :admin, %{email: "bernd@berndes.com"})
     comment_attrs = %{message: "Hello!", article_id: article.id, user_id: user.id}
+
     conn = post conn, comment_path(conn, :create), comment: comment_attrs
+
     assert redirected_to(conn) == article_path(conn, :show, article)
     assert Repo.get_by(Comment, comment_attrs)
     assert_email_sent to: {Scripture.UserView.full_name(admin), admin.email}
     assert_email_sent to: {Scripture.UserView.full_name(admin2), admin2.email}
   end
 
+  test "creating comment does not send notification to the author", %{conn: conn, article: article} do
+    admin_author = persist_fixture(User, :admin)
+    comment_attrs = %{message: "Hello!", article_id: article.id, user_id: admin_author.id}
+
+    conn = post conn, comment_path(conn, :create), comment: comment_attrs
+
+    assert_email_not_sent to: {Scripture.UserView.full_name(admin_author), admin_author.email}
+  end
+
   test "deletes comment", %{conn: conn, user: user, article: article} do
     comment = persist_fixture(Comment, %{article_id: article.id, user_id: user.id})
+
     conn = delete conn, comment_path(conn, :delete, comment)
+
     assert redirected_to(conn) == article_path(conn, :show, article)
     refute Repo.get(Comment, comment.id)
     assert get_flash(conn) == %{"info" => "Kommentar gelöscht."}
