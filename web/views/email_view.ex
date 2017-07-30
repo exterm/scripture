64e5309b9582b6265dc0384bae603c1fe181b6f3
Philip Müller <mail@philip.in-aachen.net>
2017-07-29 23:01:55 -0400

    reformat comment notification email

--- web/views/email_view.ex
+++ web/views/email_view.ex
@@ -1,6 +1,9 @@
 defmodule Scripture.EmailView do
   use Scripture.Web, :view
 
+  import Scripture.UserView, only: [full_name: 1]
+  import Scripture.DateView, only: [formatted_datetime: 1]
+
   def login_link(token, requested_path) do
     String.trim_trailing(article_url(Scripture.Endpoint, :index), "/") <> requested_path <> "?login_token=" <> token
   end
