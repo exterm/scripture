16b83d907dca27f1066bfc1e1255020e9b2dbde0
Philip Müller <mail@philip.in-aachen.net>
2020-04-12 15:14:42 -0400

    remove extraneous generated routes

--- lib/scripture_web/router.ex
+++ lib/scripture_web/router.ex
@@ -9,18 +9,9 @@ defmodule ScriptureWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", ScriptureWeb do
     pipe_through :browser
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", ScriptureWeb do
-  #   pipe_through :api
-  # end
 end
